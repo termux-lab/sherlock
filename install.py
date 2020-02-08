8a1fe174a699d440b516b7e86bef364c3425dc36
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-02-08 05:43:41 +0300

    Update install.py

--- install.py
+++ install.py
@@ -10,17 +10,17 @@ while True:
         os.system("pip install colorama")
         os.system("rm i.tl")
         save_file = open("sher.sh", "w+")
-        save_file.write("python3 sherlock/sherl.py")
+        save_file.write("python3 sherl.py")
         save_file.close()
-        os.system("cat sher.sh > /data/data/com.termux/files/usr/bin/sher")
-        os.system("chmod 700 /data/data/com.termux/files/usr/bin/sher")
+        os.system("cat sher.sh > /data/data/com.termux/files/usr/bin/sherl")
+        os.system("chmod 700 /data/data/com.termux/files/usr/bin/sherl")
         os.system("cd ..")
         break
     elif t == '0':
         os.system("pkg install wget")
         os.system("wget https://raw.githubusercontent.com/termux-lab/sherlock/master/sherl.py")
         os.system("wget https://raw.githubusercontent.com/termux-lab/sherlock/master/index.php")
-        os.system("python3 sherl.py")
+        os.system("sherl")
         break
     else:
         print("0/1 ???")
